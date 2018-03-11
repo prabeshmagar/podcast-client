7192af0d519cf3ec168f47b17ac66a65318ccf4f
prabesh magar <prabesh344@gmail.com>
2018-03-11 23:04:50 +0545

    dependencies updated

---
+++ src/components/player/Player.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="player">
+    <div class="player__header">
+      <span>Now playing</span>: {{ podcast.title }}
+    </div>
+    <audio controls autoplay ref="player" class="player__audio">
+     <source type="audio/mp3" v-if="podcast.files.mp3" :src="podcast.files.mp3">
+     <source type="audio/mp3" v-if="podcast.files.ogg" :src="podcast.files.ogg">
+      Your browser does not support the audio element.
+    </audio>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: [
+      'podcast'
+    ],
+    watch: {
+      podcast () {
+        this.$refs.player.load()
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .player {
+    width: 100%;
+
+    &__audio {
+      width: 100%;
+    }
+
+    &__header {
+      font-weight: 500px;
+      margin-bottom: 20px;
+
+      span {
+        font-weight: 800;
+      }
+    }
+  }
+</style>
